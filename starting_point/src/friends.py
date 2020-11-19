339ba9eb251d5b012bed0d710dbbf2e268c667a0
Bela <belahamid@gmail.com>
2020-11-19 12:00:55 +0000

    3rd test

--- starting_point/src/friends.py
+++ starting_point/src/friends.py
@@ -2,4 +2,18 @@ def get_name(person):
     return person["name"]
 
 def get_favourite_tv_show(person):
-    return person["favourite"]["tv_show"]
+    return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    for snack in person["favourites"]["snacks"]:
+        if snack == food:
+            return True
+  
+    return False
+        
+
+        
+        
+
+
+
