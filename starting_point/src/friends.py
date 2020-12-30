cfb5192dccc23bc88d0efb7fbf98c518ec34834f
Bela <belahamid@gmail.com>
2020-12-30 23:29:18 +0000

    added two new functions

--- starting_point/src/friends.py
+++ starting_point/src/friends.py
@@ -4,19 +4,20 @@ def get_name(person):
 def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
 
+
 def likes_to_eat(person, food):
     for snack in person["favourites"]["snacks"]:
         if snack == food:
             return True
-  
     return False
+
         
 def add_friend(person, new_friend):
     person["friends"].append(new_friend)
         
 def remove_friend(person, ex_friend):
     person["friends"].remove(ex_friend)
-        
+    
 def total_money(people):
     wallet = []
     for person in people:
@@ -24,12 +25,22 @@ def total_money(people):
         total_wallet = sum(wallet)
     return total_wallet
 
-def loan(lender, lendee, amount):
-    lender["monies"] = lender["monies"] - amount
+def l_money(lender, lendee, amount):
+    lender["monies"]  = lender["monies"] - amount
     lendee["monies"] = lendee["monies"] + amount
     print(lender["monies"])
     print(lendee["monies"])
 
 
+def all_favourite_foods(people):
+    favourite_snacks=[]
+    for person in people:
+        favourite_snacks.extend(person["favourites"]["snacks"])
+    return favourite_snacks
 
-
+def find_no_friendends(people):
+    no_friends =[]
+    for person in people:
+        if person["friends"] == []:
+            no_friends.append(person)
+    return no_friends
--- starting_point/tests/friends_test.py
+++ starting_point/tests/friends_test.py
@@ -70,7 +70,7 @@ class TestFriends(unittest.TestCase):
 
   # 2. For a given person, return their favourite tv show
   # (e.g. the function favourite_tv_show(self.person2) should return the string "Baywatch")
-  #@unittest.skip("delete this line to run the test")  
+  # @unittest.skip("delete this line to run the test")  
   def test_get_favourite_tv_show(self):
     result = get_favourite_tv_show(self.person2)
     self.assertEqual("Baywatch", result)
@@ -78,11 +78,11 @@ class TestFriends(unittest.TestCase):
 
   # 3. For a given person, check if they like a particular food
   # (e.g. the function likes_to_eat(self.person2, "bread") should return True, likes_to_eat(self.person3, "spinach") should return False)
-  #@unittest.skip("delete this line to run the test")
+  # @unittest.skip("delete this line to run the test")
   def test_person_likes_food__True(self):
     self.assertEqual(True, likes_to_eat(self.person2, "bread"))
 
-  #@unittest.skip("delete this line to run the test")
+  # @unittest.skip("delete this line to run the test")
   def test_person_likes_food__False(self):
     self.assertEqual(False, likes_to_eat(self.person3, "spinach"))
 
@@ -90,7 +90,7 @@ class TestFriends(unittest.TestCase):
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(self.person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
   # (hint: This function should not return anything. After the function call, check for the length of the friends array to test it!)
-  #@unittest.skip("delete this line to run the test")
+  # @unittest.skip("delete this line to run the test")
   def test_add_friend(self):
     add_friend(self.person2, "Scrappy-Doo")
     self.assertEqual(2, len(self.person2["friends"]))
@@ -98,7 +98,7 @@ class TestFriends(unittest.TestCase):
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
-  #@unittest.skip("delete this line to run the test")
+  # @unittest.skip("delete this line to run the test")
   def test_remove_friend(self):
     add_friend(self.person2, "Scrappy-Doo")
     remove_friend(self.person2, "Fred")
@@ -107,7 +107,7 @@ class TestFriends(unittest.TestCase):
 
   # 6. Find the total of everyone's money
   # (hint: use the self.people array, remember how we checked the total number of eggs yesterday?)
-  #@unittest.skip("delete this line to run the test")
+  # @unittest.skip("delete this line to run the test")
   def test_total_money(self):
     self.assertEqual(143, total_money(self.people))
 
@@ -124,7 +124,7 @@ class TestFriends(unittest.TestCase):
 
   # 8. Find the set of everyone's favourite food joined together
   # (hint: concatenate the favourites/snack arrays together)
-  @unittest.skip("delete this line to run the test")
+  #@unittest.skip("delete this line to run the test")
   def test_favourite_foods(self):
     result = all_favourite_foods(self.people)
     expected = ["charcuterie", "soup", "bread", "Scooby snacks", "spaghetti", "ratatouille", "spinach"]
@@ -133,6 +133,6 @@ class TestFriends(unittest.TestCase):
 
   # 9. Find people with no friends
   # (hint: return an array, there might be more people in the future with no friends!)
-  @unittest.skip("delete this line to run the test")
+  #@unittest.skip("delete this line to run the test")
   def test_find_no_friendends(self):
     self.assertEqual([self.person5], find_no_friendends(self.people))
